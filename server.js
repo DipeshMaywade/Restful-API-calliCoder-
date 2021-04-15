8244be8407a50e2a101b781e27470366379c1a72
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-04-15 21:16:10 +0530

    modified: abilty perform CRUD opration

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.get('/', (req, res) => {
     res.send("Welcome to the App");
 });
 
+require('./app/routes/note.routes.js')(app);
+
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
